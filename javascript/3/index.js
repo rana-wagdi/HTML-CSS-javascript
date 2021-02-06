188c32e17527367a488db2556add44ab04b25a71
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-06 04:18:31 -0800

    finish game

--- javascript/3/index.js
+++ javascript/3/index.js
@@ -13,3 +13,11 @@ var randomNumber2 = pic[Math.floor(Math.random() * pic.length)];
 
 document.querySelector(".img1").src=randomNumber1;
 document.querySelector(".img2").src = randomNumber2;
+
+if (randomNumber1>randomNumber2){
+    document.querySelector('h1').innerHTML="Player 1 winner";
+}else if (randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 winner!";
+}
